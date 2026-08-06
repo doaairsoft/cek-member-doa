10b84e73cbf9a2cb45bc634b418d418f78f5caf7
doaairsoft <doaairsoft08@gmail.com>
2026-08-06 13:40:35 +0700

    Update app.js2

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
+function login(){
 
-async function login(){
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-const nia=document.getElementById("nia").value;
+    if(username==="" || password===""){
+        alert("Lengkapi data terlebih dahulu.");
+        return;
+    }
 
-const password=document.getElementById("password").value;
-
-document.getElementById("status").innerHTML="Loading...";
-
-const url=`${CONFIG.API_URL}?action=login&nia=${nia}&password=${password}`;
-
-try{
-
-const res=await fetch(url);
-
-const data=await res.json();
-
-if(data.status){
-
-document.getElementById("status").innerHTML="Login berhasil";
-
-}else{
-
-document.getElementById("status").innerHTML="Login gagal";
-
-}
-
-}catch(e){
-
-document.getElementById("status").innerHTML="Server Error";
-
-}
+    alert("Login berhasil (sementara).");
 
 }
