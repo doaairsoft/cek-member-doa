662004d473112d62abfde83d32d45e7ae264b5bc
doaairsoft <doaairsoft08@gmail.com>
2026-08-06 13:13:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 
+async function login(){
+
+const nia=document.getElementById("nia").value;
+
+const password=document.getElementById("password").value;
+
+document.getElementById("status").innerHTML="Loading...";
+
+const url=`${CONFIG.API_URL}?action=login&nia=${nia}&password=${password}`;
+
+try{
+
+const res=await fetch(url);
+
+const data=await res.json();
+
+if(data.status){
+
+document.getElementById("status").innerHTML="Login berhasil";
+
+}else{
+
+document.getElementById("status").innerHTML="Login gagal";
+
+}
+
+}catch(e){
+
+document.getElementById("status").innerHTML="Server Error";
+
+}
+
+}
