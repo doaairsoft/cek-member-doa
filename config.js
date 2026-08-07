10dc7219d7b5dc94bb3bd140edb18d8445747a98
doaairsoft <doaairsoft08@gmail.com>
2026-08-07 13:50:33 +0700

    Update config.js12

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // =========================
 
 const CONFIG = {
-  API_URL: https://script.google.com/macros/s/AKfycbzF2o0PabSSfu9Ct0l0PA-lcC08-q0XnLKAKvkfNgUgsOea2UsUYn2SxNASjHvyQC4L/exec
+  API_URL: "https://script.google.com/macros/s/AKfycbzF2o0PabSSfu9Ct0l0PA-lcC08-q0XnLKAKvkfNgUgsOea2UsUYn2SxNASjHvyQC4L/exec"
 };
