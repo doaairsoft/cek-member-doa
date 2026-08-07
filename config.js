c95989c768e2fd1d5318cef198b03d83348957c5
doaairsoft <doaairsoft08@gmail.com>
2026-08-07 13:58:00 +0700

    Update config.js123

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // ===================================
-// D.O.A AIRSOFT
-// config.js
+// KONFIGURASI
 // ===================================
 
 const API_URL = "https://script.google.com/macros/s/AKfycbxneWPqLFKQecungoXaOVzbaTrzxv-aVqOoGy7ucZQJEuJ-_eoDmiT-83wLa-8Ss4sd/exec";
